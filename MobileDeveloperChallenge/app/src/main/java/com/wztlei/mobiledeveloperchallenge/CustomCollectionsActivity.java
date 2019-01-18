2c548ca2d636f068a884db1dfb23488716b62864
wztlei <w.lei.thunderbolt@gmail.com>
2019-01-18 08:50:24 -0500

    Initial commit

---
+++ MobileDeveloperChallenge/app/src/main/java/com/wztlei/mobiledeveloperchallenge/CustomCollectionsActivity.java
@@ -0,0 +1,13 @@
+package com.wztlei.mobiledeveloperchallenge;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CustomCollectionsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_custom_collections);
+    }
+}
